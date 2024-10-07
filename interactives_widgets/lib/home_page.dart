f56be2f5e5a60770e4060f015c3b4ff445acfde6
devfelicity <feliciteteghomo@gmail.com>
2024-10-07 08:48:25 +0100

    [IMP] StatefullWidget

---
+++ interactives_widgets/lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+    );
+  }
+}
