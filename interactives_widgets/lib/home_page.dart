98acbe95ddcc72dbd35283ff56845bd8079a2b54
devfelicity <feliciteteghomo@gmail.com>
2024-10-09 09:17:24 +0100

    [IMP]Popup menu button

--- interactives_widgets/lib/home_page.dart
+++ interactives_widgets/lib/home_page.dart
@@ -14,126 +14,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Color backgroundColor = Colors.white;
-  List<String> _titles = [
-    "Learn interactives widgets",
-    "Learn flutter"
-  ];
-  int _titleIndex = 0;
-  int _count = 0;
-  double _iconSize = 16;
 
-  _updateCount(){
-    setState(() {
-      _count++;
-    });
-  }
-
-  _update_LongPressed() {
-    setState(() {
-      backgroundColor = (backgroundColor == Colors.white)
-          ? Colors.deepOrangeAccent
-          : Colors.white;
-    });
-  }
-CrossAxisAlignment _alignement = CrossAxisAlignment.center;
-
-  _setToStart() {
-  setState(() {
-    _alignement = CrossAxisAlignment.start;
-  });
-}
-_setToEnd() {
-  setState(() {
-    _alignement = CrossAxisAlignment.end;
-  });
-}
+  List<String> values = ["Poulet", "Boeuf", "Porc"];
+  String _selected = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_titles[_titleIndex]),
+        title: Text(widget.title),
+        actions: [
+          PopupMenuButton <String>(
+            icon: const Icon(Icons.settings),
+            onSelected: (newValue){
+              setState(() {
+                _selected = newValue;
+              });
+            },
+              itemBuilder: (context){
+                return values.map((viande) {
+                  return PopupMenuItem<String>(
+                      value: viande,
+                    child: Text(viande),
+                  );
+                }).toList();
+              }
+          )
+        ],
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      backgroundColor: backgroundColor,
       body: Center(
-        child: Column(
-          crossAxisAlignment: _alignement,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                setState(() {
-                  backgroundColor = (backgroundColor == Colors.white)
-                      ? Colors.black54
-                      : Colors.white;
-                });
-              },
-              onLongPress: _update_LongPressed,
-              child: const Text("TextButton"),
-            ),
-            TextButton.icon(
-                onPressed: _update_LongPressed,
-                label: const Icon(Icons.palette)),
-
-            ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    int newRandomTitle = Random().nextInt(_titles.length);
-                    _titleIndex = newRandomTitle;
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.deepOrange
-                ),
-                child: const Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.edit),
-                    Text("Changer le titre")
-                  ],
-                )
-            ),
-
-            FilledButton(
-                onPressed: _setToStart,
-                child: const Text("Start")
-            ),
-            FilledButton(
-                onPressed: _setToEnd,
-                child: const Text("End")
-            ),
-            FilledButton.icon(
-                onPressed: (){
-                  setState(() {
-                    _alignement = CrossAxisAlignment.center;
-                  });
-                },
-                label: const Icon(Icons.restart_alt)
-            ),
-            OutlinedButton(
-                onPressed: _updateCount,
-                style: OutlinedButton.styleFrom(
-                  side: const BorderSide(
-                    width: 2,
-                    color: Colors.redAccent,
-                    style: BorderStyle.solid
-                  ),
-                ),
-                child: Text(("Jai appuyé le bouton $_count fois"))
-            ),
-            IconButton(
-                onPressed: (){
-                  setState(() {
-                    _iconSize = Random().nextInt(250).toDouble();
-                  });
-                },
-                icon: Icon(Icons.add, size: _iconSize)
-            )
-          ],
-        ),
+        child: Text(_selected),
       ),
     );
   }
