f9666c77610f15975830f5a783127c6297505b37
devfelicity <feliciteteghomo@gmail.com>
2024-10-11 15:11:11 +0100

    [IMP] FocusScope & FocusNodde

--- interactives_widgets/lib/home_page.dart
+++ interactives_widgets/lib/home_page.dart
@@ -12,7 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String _simple="";
+  String _simple = "";
+  final FocusNode _focus = FocusNode();
+  final FocusNode _nextFocus = FocusNode();
+
+
+  // ces méthodes permettent de fermer le clavier sans avoir à faire le unsubmitted
+  void unFocusMethod1() {
+    FocusScope.of(context).unfocus();
+  }
+
+  void unFocusMethod2() {
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+  void unFocusMethod3(FocusNode focusNode) {
+    focusNode.unfocus();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,27 +37,34 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Text("Textfield simple: $_simple"),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.text,
-              obscureText: true,
-              onChanged: (newString) {
-                setState(() {
-                  _simple = newString;
-                });
-              },
-              onSubmitted: (submittedString){
-                _simple = submittedString;
-              },
+      body: InkWell(
+          onTap: () {
+            unFocusMethod3(_focus);
+          },
+          child: Center(
+            child: Column(
+              children: [
+                Text("Textfield simple: $_simple"),
+                TextField(
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  onChanged: (newString) {
+                    setState(() {
+                      _simple = newString;
+                    });
+                  },
+                  onSubmitted: (submittedString) {
+                    _simple = submittedString;
+                  },
+                  focusNode: _focus,
+                ),
+                TextField(
+                  focusNode: _nextFocus,
+                )
+              ],
             ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
-
