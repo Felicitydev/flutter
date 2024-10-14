fd5b5d69769fc30dcae1bfe49ffa1755effa9552
devfelicity <feliciteteghomo@gmail.com>
2024-10-14 13:51:15 +0100

    [IMP] Datepicker & Timepicker

--- interactives_widgets/lib/home_page.dart
+++ interactives_widgets/lib/home_page.dart
@@ -12,46 +12,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DateTime? _datePicked = DateTime.now();
+  TimeOfDay _timePicked = TimeOfDay.now();
 
-  List<User> users = [
-    User("Boland", Colors.red),
-    User("Maelys", Colors.blue),
-    User("Maissane", Colors.purple),
-    User("Yohane", Colors.greenAccent),
-    User("Maxime", Colors.black38)
-  ];
+  Future<void> _pickDate() async {
+    final newDate = await showDatePicker(
+      context: context,
+      firstDate: DateTime(1980),
+      lastDate: DateTime(3000),
+      currentDate: DateTime.now(),
+    );
+    if (newDate != null && newDate != _datePicked) {
+      setState(() {
+        _datePicked = newDate;
+      });
+    }
+  }
+
+  Future<void> _pickTime() async {
+    final newTime = await showTimePicker(
+      context: context,
+      initialTime: _timePicked,
+    );
+    if (newTime != null && newTime != _timePicked) {
+      setState(() {
+        _timePicked = newTime;
+      });
+    }
+  }
+
+  String readableDate(DateTime? date) {
+    if (date == null) {
+      return "Aucune date choisie";
+    } else {
+      final year = date.year;
+      final month = date.month;
+      final day = date.day;
+      return "La date choisie est : $day/$month/$year";
+    }
+  }
+
+  String readableTime() {
+    final hour = _timePicked.hour;
+    final minute = _timePicked.minute;
+    return "$hour:$minute";
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
       body: Center(
-        child: Wrap(
-          children: users.map((user) {
-            return Chip(
-              label: Text(user.name),
-              avatar: CircleAvatar(
-                backgroundColor: user.color,
-                child: Text(user.name[0]),
-              ),
-              onDeleted: () {
-                users.remove(user);
-              },
-            );
-          }).toList()
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(readableDate(_datePicked)),
+            TextButton(
+              onPressed: _pickDate,
+              child: const Text("Modifier la date"),
+            ),
+            Text(readableTime()),
+            TextButton(
+              onPressed: _pickTime,
+              child: const Text("Modifier l'heure"),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-class User {
-  String name;
-  Color color;
-
-  User(this.name, this.color);
 }
