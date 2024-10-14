1a794aed4e64a708447f2ee546bfa94ba6297ae7
devfelicity <feliciteteghomo@gmail.com>
2024-10-14 10:11:54 +0100

    [IMP] Le Checkbox

--- interactives_widgets/lib/home_page.dart
+++ interactives_widgets/lib/home_page.dart
@@ -18,90 +18,45 @@ class _HomePageState extends State<HomePage> {
   double current = 33;
   FavoritePlat? favoritePlat;
 
+  Map<String, bool> groceries = {
+    "Banane": false,
+    "Oeufs": false,
+    "Pates": false,
+    "Beurre": false,
+    "Poulet": false,
+    "Mayonnaise": false,
+    "Céréales": false,
+    "Café": false,
+    "Persil": false,
+    "Courgettes": false,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      backgroundColor: isOn ? Colors.white : Colors.black38,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Couleur de background: ${isOn ? 'Light' : "Dark"}"),
-                Switch(
-                    value: isOn,
-                    onChanged: (newBool) {
-                      setState(() {
-                        isOn = newBool;
-                      });
-                    },
-                  activeColor: Colors.redAccent,
-                  inactiveThumbColor: Colors.orangeAccent,
-                  inactiveTrackColor: Colors.lightBlueAccent,
-                  activeTrackColor: Colors.purple,
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Text(min.toString()),
-                Expanded(
-                    child: Slider(
-                      min: min,
-                        max: max,
-                        value: current,
-                        onChanged: (newValue) {
-                          setState(() {
-                            current = newValue;
-                          });
-                        },
-                      divisions: 5,
-                      activeColor: Colors.blue,
-                      inactiveColor: Colors.black12,
-                    )
-                ),
-                Text(max.toString())
-
-              ],
-            ),
-            Text(("Valeur du slider = ${current.toInt()}")),
-            Column(
-              children: [
-                const Text("Le meilleur plat du monde"),
-                Column(
-                  children: FavoritePlat.values.map((plat){
-                    return Row(
-                      children: [
-                        Text(favoritePlat?.name ?? "Aucun plat sélectionné!"),
-                        Radio(
-                          activeColor: Colors.lightGreen,
-                            value: plat,
-                            groupValue: favoritePlat,
-                            onChanged: (plat){
-                              setState(() {
-                                favoritePlat = plat;
-                              });
-                            }
-                        )
-                      ],
-                    );
-                  }).toList()
-                )
-
-              ],
-            )
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         ),
-      )
-    );
+        backgroundColor: isOn ? Colors.white : Colors.black38,
+        body: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: groceries.keys.map((key) {
+                return CheckboxListTile(
+                    title: Text(key),
+                    subtitle:
+                        Text(groceries[key]! ? "Dans le panier " : "A acheter"),
+                    value: groceries[key],
+                    onChanged: (bol) {
+                      setState(() {
+                        groceries[key] = bol ?? false;
+                      });
+                    });
+              }).toList()),
+        ));
   }
 }
 
-enum FavoritePlat {Okok, Eru, Poulet_DG, Met_de_pistache}
+enum FavoritePlat { Okok, Eru, Poulet_DG, Met_de_pistache }
