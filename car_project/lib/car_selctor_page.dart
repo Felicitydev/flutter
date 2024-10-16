95c16698eda9ff5ce5d5b1ad62984665f5470ba1
devfelicity <feliciteteghomo@gmail.com>
2024-10-16 10:05:56 +0100

    [IMP] Define textfile

--- car_project/lib/car_selctor_page.dart
+++ car_project/lib/car_selctor_page.dart
@@ -12,6 +12,36 @@ class CarSelectorPage extends StatefulWidget {
 
 class _ClassSelectorPageState extends State<CarSelectorPage> {
 
+  String _firstName = "";
+
+  Padding _interactiveWidget({required List<Widget> children, bool isRow = false}) {
+    return Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            (isRow) ?
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: children,
+              )
+            : Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: children,
+            ),
+            const Divider(),
+          ],
+        ),
+    );
+  }
+
+  void _updateFirstName(String newValue){
+    setState(() {
+      _firstName = newValue;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +54,30 @@ class _ClassSelectorPageState extends State<CarSelectorPage> {
           )
         ],
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+                "Bienvenu.e: $_firstName",
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.lightBlue
+              ),
+            ),
+            _interactiveWidget(
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Entrez votre nom"
+                    ),
+                    onSubmitted: _updateFirstName
+                  )
+                ]
+            )
+          ],
+        ),
+      ),
     );
   }
 }
