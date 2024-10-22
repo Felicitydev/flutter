12c3ed13c6cd040cb3325526ad1cbb010a17e5c4
devfelicity <feliciteteghomo@gmail.com>
2024-10-22 10:12:35 +0100

    [IMP] Tae a photo and add to gallery

--- package/lib/main.dart
+++ package/lib/main.dart
@@ -15,13 +15,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Mes photos',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       darkTheme: ThemeData.dark(useMaterial3: true),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Mes photos'),
     );
   }
 }
@@ -35,17 +35,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  File? _file;
 
-  Future<void> _pickImage(ImageSource source) async{
-    XFile? xFile = await ImagePicker().pickImage(source: source);
-    if (xFile != null){
+  List<File> _images = [];
+
+  Future<void> _pickImage({required ImageSource source}) async {
+    ImagePicker picker = ImagePicker();
+    XFile? xFile = await picker.pickImage(source: source);
+    if(xFile != null){
       setState(() {
         String path = xFile.path;
-        _file = File(path);
+        File? file = File(path);
+        if(file != null){
+          _images.add(file);
+        }
       });
     }
   }
+
+  void _removePicture(File file){
+    setState(() {
+      _images.remove(file);
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -53,26 +65,44 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (() => _pickImage(source: ImageSource.gallery)), 
+            icon: const Icon(Icons.photo_album)
+          ),
+          IconButton(
+            onPressed: (() => _pickImage(source: ImageSource.camera)), 
+            icon: const Icon(Icons.camera_alt)
+          ),
+        ],
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            _file != null
-                ? Image.file(_file!)
-                : FlutterLogo(size: MediaQuery.of(context).size.width * 0.8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                    onPressed: (() => _pickImage(ImageSource.camera)),
-                    icon: const Icon(FontAwesomeIcons.camera, color: Colors.blue,)),
-                IconButton(
-                    onPressed: (() => _pickImage(ImageSource.gallery)),
-                    icon: const Icon(FontAwesomeIcons.photoFilm, color: Colors.deepPurple,)),
-              ],
-            )
-          ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              SizedBox(
+                height: 128,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: _images.map((img){
+                      return InkWell(
+                        onLongPress: (() => _removePicture(img)),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: CircleAvatar(
+                          radius: 54,
+                          backgroundImage: FileImage(img),
+                          ),
+                        )
+
+                      );
+                    }).toList(),
+                  ),
+                ),
+              )
+            ],
         ),
       ),
     );
